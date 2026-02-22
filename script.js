3e0a2c75287bf33f0033b9fa0c79c60d64af0b33
Hng890-website <trinhtooanh@gmail.com>
2026-02-22 14:01:55 +0700

    Refactor game status messages and engine depth

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var board = null;
 var game = new Chess();
 var engine;
 
-// 1. Giải quyết lỗi Security bằng Blob + importScripts
+// Khởi tạo Stockfish
 const workerCode = `importScripts('https://cdnjs.cloudflare.com/ajax/libs/stockfish.js/10.0.2/stockfish.js');`;
 const blob = new Blob([workerCode], { type: 'application/javascript' });
 engine = new Worker(URL.createObjectURL(blob));
@@ -12,41 +12,37 @@ engine.onmessage = function(e) {
         const move = e.data.split(' ')[1];
         game.move(move, { sloppy: true });
         board.position(game.fen());
-        $('#engine-status').text('Máy đã đi xong');
-        $('#user-status').text('Đến lượt bạn');
+        $('#engine-status').text('Máy vừa đi xong. Tới bạn!');
+        $('#user-status').text('Tới lượt bạn đi');
     }
 };
 
 engine.postMessage('uci');
 engine.postMessage('isready');
 
-// 2. Hàm xử lý kéo thả
 function onDrop(source, target) {
     const move = game.move({ from: source, to: target, promotion: 'q' });
     if (move === null) return 'snapback';
 
-    $('#user-status').text('Đã đi xong');
-    $('#engine-status').text('Máy đang nghĩ...');
+    $('#user-status').text('Chờ máy tính...');
+    $('#engine-status').text('Máy đang suy nghĩ...');
     
     engine.postMessage('position fen ' + game.fen());
-    engine.postMessage('go depth 10');
+    engine.postMessage('go depth 12');
 }
 
-// 3. Khởi tạo bàn cờ với HÌNH ẢNH QUÂN CỜ
-var config = {
+board = Chessboard('myBoard', {
     draggable: true,
     position: 'start',
     pieceTheme: 'https://chessboardjs.com/img/chesspieces/wikipedia/{piece}.png',
     onDrop: onDrop,
     onSnapEnd: () => board.position(game.fen())
-};
-board = Chessboard('myBoard', config);
+});
 
-// 4. Nút ván mới
-$('#resetBtn').on('click', function() {
+$('#resetBtn').on('click', () => {
     game.reset();
     board.start();
     engine.postMessage('ucinewgame');
-    engine.postMessage('isready');
-    $('#user-status').text('Ván mới bắt đầu!');
+    $('#engine-status').text('Máy đã sẵn sàng!');
+    $('#user-status').text('Tới lượt bạn đi');
 });
